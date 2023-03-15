bba2f1f5585d2a55eed5018ee55577d1481df145
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 01:34:28 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,12 @@
- import React from 'react';
+import React from 'react';
 
- export const Todo  = () =>{
-    
+ export const Todo  = (props) =>{
+    return (
+        <>
+            {props.list.map((elem)=><div className='todo' id={'todo-'+elem.id} key={elem.id+"ky"}>
+                <div className='todo-title todo-text'>{elem.title}</div>
+                <div className='todo-status'>{elem.completed?"Complete":"Incomplete"}</div>
+                </div>)}
+        </>
+    )
  }
